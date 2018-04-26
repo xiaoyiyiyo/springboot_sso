edbbd4d1f0486d75e5335c48302f92a96645f677
xiaoyiyiyo <xiaoyiyiyo@outlook.com>
2018-04-26 23:45:29 +0800

    add redis config

---
+++ sso_server/src/main/java/com/xiaoyiyiyo/configuration/RedisConfiguration.java
@@ -0,0 +1,42 @@
+package com.xiaoyiyiyo.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Created by xiaoyiyiyo on 2018/4/26.
+ */
+@Configuration
+@PropertySource(value="classpath:/redis.properties")
+public class RedisConfiguration {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Value("${spring.redis.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Bean
+    public JedisPool redisPoolFactory() {
+        System.out.println("JedisPool注入成功！！");
+        System.out.println("redis地址：" + host + ":" + port);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port);
+        return jedisPool;
+    }
+}
