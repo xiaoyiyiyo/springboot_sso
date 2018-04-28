91bdb362388ff64ca8b6be2ddc2513f531d5f8ef
xiaoyiyiyo <xiaoyiyiyo@outlook.com>
2018-04-28 16:24:01 +0800

    springboot2.0 redis 缓存 配置

---
+++ sso_server/src/main/java/com/xiaoyiyiyo/configuration/RedisCacheConfig.java
@@ -0,0 +1,101 @@
+package com.xiaoyiyiyo.configuration;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+/**
+ * Created by xiaoyiyiyo on 2018/4/28.
+ */
+@EnableCaching
+@Configuration
+public class RedisCacheConfig extends CachingConfigurerSupport{
+
+    /*@Bean
+    public RedisTemplate redisTemplate() {
+
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        //全局开关，支持jackson在反序列是使用多态
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        StringRedisTemplate redisTemplate = new StringRedisTemplate();
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        return redisTemplate;
+
+    }*/
+
+   /* @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager manager = new RedisCacheManager(redisTemplate);
+        manager.setUsePrefix(true);
+        RedisCachePrefix cachePrefix = new RedisPrefix("prefix");
+        manager.setCachePrefix(cachePrefix);
+        // 整体缓存过期时间
+        manager.setDefaultExpiration(3600L);
+        // 设置缓存过期时间。key和缓存过期时间，单位秒
+        Map<String, Long> expiresMap = new HashMap<>();
+        expiresMap.put("user", 1000L);
+        manager.setExpires(expiresMap);
+        return manager;
+    }*/
+
+    @Bean
+    public Jackson2JsonRedisSerializer jackson2JsonRedisSerializer () {
+
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        //全局开关，支持jackson在反序列是使用多态
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        return jackson2JsonRedisSerializer;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
+        RedisSerializationContext.SerializationPair<Object> pair =
+               RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer());
+
+        RedisCacheConfiguration redisCacheConfiguration =
+               RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(pair);
+        //设置默认超过期时间是30秒
+        redisCacheConfiguration.entryTtl(Duration.ofSeconds(30));
+        //初始化RedisCacheManager
+        RedisCacheManager cacheManager = new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+        return cacheManager;
+    }
+
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+        return (target, method, objects) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append("::" + method.getName() + ":");
+            for (Object obj : objects) {
+                sb.append(obj.toString());
+            }
+            return sb.toString();
+        };
+    }
+}
